889bcd0207b455004750bf1212b4a7dd43347fa0
Alexandr Navrotskiy <navrotskiy.alex@gmail.com>
2022-01-06 17:36:54 +0200

    seeders update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            TaskStatusSeeder::class
+            TaskStatusSeeder::class,
+            LabelSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
     }
